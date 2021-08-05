95c1ebe48effa4e2d7df65dfe46c4fe90cc5a8fe
Adrian Gawle <61741591+agawle@users.noreply.github.com>
2021-08-05 19:43:08 +0200

    #9 - Set up Inertia (#10)
    * #9 - Set up Inertia
    
    * #9 - Set up Inertia
    
    * #9 - Set up Inertia
    
    * #9 - Set up Inertia

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Home\IndexController as HomeIndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", fn() => view("welcome"));
+Route::get("/", [HomeIndexController::class, "__invoke"]);
